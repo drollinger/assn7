ab92bd313e91fedeb2f966d3c048367f10a90d7c
drollinger <drollingerdallin@gmail.com>
2019-04-22 20:05:23 -0600

    Going to try cutting some code down

--- src/Graph.java
+++ src/Graph.java
@@ -1,43 +1,26 @@
+/***********************************************************************
+ * Name: Dallin Drollinger
+ * Description: Graph.java is our main function that runs and tests our
+ *      functions for shortest ancestral path and outcast.
+ *********************************************************************/
 import java.io.File;
 import java.util.*;
 
 public class Graph {
-    int numVertex;  // Number of vertices in the graph.
-    GraphNode[] G;  // Adjacency list for graph.
-    String graphName;  //The file from which the graph was created.
+    private int numVertex;  // Number of vertices in the graph.
+    private GraphNode[] G;  // Adjacency list for graph.
+    private String graphName;  //The file from which the graph was created.
 
-
+    // Constructor to initialize a few variables
     public Graph() {
         this.numVertex = 0;
         this.graphName = "";
     }
 
-    public Graph(int numVertex) {
-        this.numVertex = numVertex;
-        G = new GraphNode[numVertex];
-        for (int i = 0; i < numVertex; i++) {
-            G[i] = new GraphNode( i );
-        }
-    }
-
-    public boolean addEdge(int source, int destination) {
-        if (source < 0 || source >= numVertex) return false;
-        if (destination < 0 || destination >= numVertex) return false;
-        //add edge
-        G[source].addEdge( source, destination );
-        return true;
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append( "The Graph " + graphName + " \n" );
-
-        for (int i = 0; i < numVertex; i++) {
-            sb.append( G[i].toString() );
-        }
-        return sb.toString();
-    }
-
+    /***********************************************
+     * Function given to create graph from .txt file
+     * @param filename .txt file
+     **********************************************/
     public void makeGraph(String filename) {
         try {
             graphName = filename;
@@ -59,67 +42,48 @@ public class Graph {
         }
     }
 
-    /**                                                                    //TODO: THIS
-     * Find the path from v1 to v2 going through anc.
-     *
-     * @param v1:  first vertex
-     * @param v2:  second vertex
-     * @param anc: ancestor of v1 and v2
-     * @return the path
-     */
-    public String reportPath(int v1, int v2, int anc) {
-        StringBuilder sb = new StringBuilder();
-        sb.append( "Wish I knew the path from " + v1 + " " + anc + " " + v2 );
-
-        return sb.toString();
-    }
-
-    /**
-     * Computes the least common ancestor of v1 and v2, prints the length of the path, the ancestor, and the path itself.    //TODO: THIS
-     *
+    /***************************************************************************
+     * Prints the length of the path, the ancestor, and the path itself.
      * @param v1: first vertex
      * @param v2: second vertex
      * @return returns the length of the shortest ancestral path.
-     */
+     **************************************************************************/
     public int lca(int v1, int v2) {
-        // Compute lca
         PathInfo best = new PathInfo(getPath(v1, v2));
-
         System.out.println( graphName + " Best lca " + v1 + " " + v2 + " Distance: " + best.dist + " Ancestor " + best.pred + " Path: " + best );
-
         return best.dist;
     }
 
+    /**********************************************************************************
+     * Private function to calculate the path. It tracks all the ancestors of
+     * v1 and then it finds the least common ancestor shared with v2
+     * @param v1: first vertex
+     * @param v2: second vertex
+     * @return a String with the path separated with spaces and a "|" char after the lca
+     ***********************************************************************************/
     private String getPath(int v1, int v2) {
         int[] nodeGraph1 = new int[numVertex];
         for(int i = 0; i < this.numVertex; i++) {
             nodeGraph1[i] = -1;
         }
-
         Queue<Integer> nodeQueue1 = new LinkedList<>();
-
         nodeQueue1.add(v1);
         nodeGraph1[v1] = numVertex + 1;
         int cur;
-
         while(!nodeQueue1.isEmpty()) {
             cur = nodeQueue1.remove();
-
             for(EdgeInfo E : this.G[cur].succ) {
                 nodeGraph1[E.to] = E.from;
                 nodeQueue1.add(E.to);
             }
         }
-
         int[] nodeGraph2 = new int[numVertex];
         for(int i = 0; i < this.numVertex; i++) {
             nodeGraph2[i] = -1;
         }
         Queue<Integer> nodeQueue2 = new LinkedList<>();
-
         nodeQueue2.add(v2);
         cur = v2;
-
         while(!nodeQueue2.isEmpty() && nodeGraph1[cur] < 0) {
             cur = nodeQueue2.remove();
 
@@ -128,10 +92,15 @@ public class Graph {
                 nodeQueue2.add(E.to);
             }
         }
-
         return path1(nodeGraph1, cur) + "|" + path2(nodeGraph2, nodeGraph2[cur]);
     }
 
+    /*******************************************************************************
+     * Formats the list path to the least common ancestor into a string
+     * @param l1: list of elements in the path
+     * @param node: the current node that is being worked on in the list
+     * @return returns the string of the path
+     *******************************************************************************/
     private String path1(int[] l1, int node) {
         if(l1[node] > this.numVertex) {
             return Integer.toString(node);
@@ -141,6 +110,12 @@ public class Graph {
         }
     }
 
+    /*******************************************************************************
+     * Formats the list path to the least common ancestor into a string
+     * @param l2: list of elements in the path
+     * @param node: the current node that is being worked on in the list
+     * @return returns the string of the path
+     *******************************************************************************/
     private String path2(int[] l2, int node) {
         if(node < 0) {
             return "";
@@ -153,37 +128,39 @@ public class Graph {
         }
     }
 
-    public int outcast(int[] v) {
-        int[] sums = new int[v.length];
+    /*******************************************************************************
+     * function is provided a list of nodes in a graph and returns the node with
+     * the largest sum of distances from other nodes.
+     * @param nodes: list of nodes to work on
+     * @return returns the node with that is considered the outcast
+     ******************************************************************************/
+    public int outcast(int[] nodes) {
+        int[] sums = new int[nodes.length];
         int maxSum = -1;
         int outcast = -1;
         int pathLength;
-
         System.out.println("\n--Outcast on " + this.graphName + "--");
         for(int i = 0; i < sums.length; i++) {
             sums[i] = 0;
         }
-
-        for(int curNode = 0; curNode < v.length - 1; curNode++) {
-            for (int otherNode = curNode + 1; otherNode < v.length; otherNode++) {
-                pathLength = lca(v[curNode], v[otherNode]);
+        for(int curNode = 0; curNode < nodes.length - 1; curNode++) {
+            for (int otherNode = curNode + 1; otherNode < nodes.length; otherNode++) {
+                pathLength = lca(nodes[curNode], nodes[otherNode]);
                 sums[curNode] += pathLength;
                 sums[otherNode] += pathLength;
             }
         }
-
         for(int i = 0; i < sums.length; i++) {
             if(sums[i] > maxSum) {
                 maxSum = sums[i];
-                outcast = v[i];
+                outcast = nodes[i];
             }
         }
-
-        System.out.println( "The outcast of " + Arrays.toString( v ) + " is " + outcast + " with distance sum of " + maxSum);
+        System.out.println( "The outcast of " + Arrays.toString( nodes ) + " is " + outcast + " with distance sum of " + maxSum);
         return outcast;
-
     }
 
+    //main function for testing
     public static void main(String[] args) {
         Graph graph1 = new Graph();
         graph1.makeGraph( "digraph1.txt" );
